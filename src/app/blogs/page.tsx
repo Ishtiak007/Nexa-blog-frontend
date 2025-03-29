31490d05926b1bc273118750ae7309506f6d1d6b
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-03-29 15:22:50 +0600

    Retrieve dynamic data with an ID for the detail page using SSR

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,11 +1,18 @@
 import BlogCard from "@/components/ui/BlogCard";
 import { Blog } from "@/types";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Nexa Blog | Blogs",
+};
 
 const BlogsPage = async () => {
-  const res = await fetch("http://localhost:5000/blogs");
+  const res = await fetch("http://localhost:5000/blogs", {
+    cache: "no-store",
+  });
   const blogs = await res.json();
   return (
-    <div>
+    <div className="w-[90%] mx-auto">
       <h1 className="text-3xl text-center my-5 font-bold">
         Expolore all <span className="text-teal-600">Blogs</span>
       </h1>
@@ -15,7 +22,7 @@ const BlogsPage = async () => {
           groundbreaking computational power is unleashed like never before.
         </i>
       </p>
-      <div>
+      <div className="grid grid-cols-3 gap-6 my-5">
         {blogs.map((blog: Blog) => (
           <BlogCard key={blog.id} blog={blog} />
         ))}
