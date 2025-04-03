bb3f192b608ecf016a8d1662bf84eb7262d60df8
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-04-03 14:36:23 +0600

    implement dynamic metadata generation for blog pages

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -11,9 +11,18 @@ export const generateStaticParams = async () => {
 };
 
 // or Dynamic metadata
-export async function generateMetadata({ params }) {
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ blogId: string }>;
+}) {
+  const { blogId } = await params;
+
+  const res = await fetch(`http://localhost:5000/blogs/${blogId}`);
+  const blog = await res.json();
+
   return {
-    title: "...",
+    title: blog.title,
   };
 }
 
